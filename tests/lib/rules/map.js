29d4a6020c5cd9280b023dadecc834962e1823d7
user <user@MacBook-Pro-user.local>
2021-06-05 22:46:15 +0300

    tests

---
+++ tests/lib/rules/map.js
@@ -0,0 +1,92 @@
+/**
+ * @fileoverview eslint-plugin-lodash-to-native
+ * @author Ignatev Pavel
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+let rule = require('../../../lib/rules/map'),
+  RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+let ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2021 } });
+ruleTester.run('map', rule, {
+  valid: [
+    //ES6
+    'let a = _.map({a : 1, b: 2}, n => {n * n})',
+    //ES5
+    'var a = _.map({a : 1, b: 2}, function(n){n * n})',
+    //Без объявления функции как переменная
+    '_.map({a : 1, b: 2}, n => {n * n})',
+    //Правило не тригерит само себя, если проверка на значение переменной массив ли это уже существует
+    'Array.isArray(variable) ? variable.map(() => {}) : _.map(variable, () => {})',
+    //После присваиная не отрабатывает
+    `_ = {map: () => []};
+    false ? true : _.map(variable, () => {})`
+    
+  ],
+
+  invalid: [
+    {
+      code: '_.map([1, 2, 3], function(n){n * n})',
+      output: "[1, 2, 3].map(function(n){n * n})",
+      errors: [
+        {
+          message: 'Замените использование _.map на нативное Array#map',
+          type: 'CallExpression'
+        }
+      ]
+    }, 
+    {
+        code: '_.map([1, 2, 3], fn)',
+        output: "[1, 2, 3].map(fn)",
+        errors: [
+          {
+            message: 'Замените использование _.map на нативное Array#map',
+            type: 'CallExpression'
+          }
+        ]
+      }, 
+    {
+        code: '_.map(variable, () => {})',
+        output: "Array.isArray(variable) ? variable.map(() => {}) : _.map(variable, () => {})",
+        errors: [
+          {
+            message: 'Замените использование _.map на нативное Array#map, если первый параметр массив',
+            type: 'CallExpression'
+          }
+        ]
+      }, 
+      {
+        code: 'false ? true : _.map(variable, () => {})',
+        output: "false ? true : Array.isArray(variable) ? variable.map(() => {}) : _.map(variable, () => {})",
+        errors: [
+          {
+            message: 'Замените использование _.map на нативное Array#map, если первый параметр массив',
+          }
+        ]
+      }, 
+      {
+        code: `false ? true : _.map(variable, () => {});
+        _ = {map: () => []};
+        false ? true : _.map(variable, () => {})`,
+        output: `false ? true : Array.isArray(variable) ? variable.map(() => {}) : _.map(variable, () => {});
+        _ = {map: () => []};
+        false ? true : _.map(variable, () => {})`,
+        errors: [
+          {
+            message: 'Замените использование _.map на нативное Array#map, если первый параметр массив',
+          }
+        ]
+      }, 
+
+
+
+  ]
+});
