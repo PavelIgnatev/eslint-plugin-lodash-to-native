717b956d3a1005d9258e73fdf3ec8e6cdab1f90c
user <user@MacBook-Pro-user.local>
2021-06-05 20:52:07 +0300

    eslint-plugin-lodash-to-native

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview eslint-plugin-lodash-to-native
+ * @author IgnatevPavel
+ */
+ 'use strict';
+
+ module.exports = {
+   rules: {
+     map: require('./lib/rules/map'),
+   },
+ };
